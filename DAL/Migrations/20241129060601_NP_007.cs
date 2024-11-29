cb5847b5cf97da23cfe348b3b12e85b6bf9a5d74
NileshPatel <Nilesh.Patel@devstree.in>
2024-11-29 12:11:03 +0530

    Report form

---
+++ DAL/Migrations/20241129060601_NP_007.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class NP_007 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "SYS_Reports",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Type = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CreatedBy = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    UpdatedBy = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    UpdatedDate = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    DeleteStatus = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SYS_Reports", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SYS_Reports");
+        }
+    }
+}
---
+++ UI/Controllers/ReportsController.cs
@@ -0,0 +1,87 @@
+﻿using BL.Constants;
+using BL.Models.Administration;
+using BL.Models.Common;
+using BL.Services.Administration;
+using BL.Services.Common;
+using DAL.Context;
+using DAL.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using UI.Controllers.Common;
+
+namespace UI.Controllers
+{
+    public class ReportsController : BaseController
+    {
+        public ReportsService service;
+        public ReportsController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, RoleManager<ApplicationRole> rolemanager,
+            PerspectiveContext Dbcontext) : base(userManager, signInManager, rolemanager, Dbcontext)
+        {
+            service = new ReportsService();
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult<BaseResponseDTO<List<ReportsDTO>>> GetAll()
+        {
+            try
+            {
+                BaseResponseDTO<List<ReportsDTO>> dt = new BaseResponseDTO<List<ReportsDTO>>();
+                dt = service.GetAll();
+                return Ok(dt);
+            }
+            catch (Exception)
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<BaseResponseDTO<ReportsDTO>>> GetById(string Id)
+        {
+            try
+            {
+                BaseResponseDTO<ReportsDTO> dt = new BaseResponseDTO<ReportsDTO>();
+                dt.Data = new ReportsDTO();
+                if (!string.IsNullOrEmpty(Id))
+                {
+                    dt = service.GetById(Id);
+                }
+
+                return Ok(dt);
+            }
+            catch (Exception)
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<BaseResponseDTO<bool>>> CreateUpdate(ReportsDTO dto)
+        {
+            try
+            {
+                BaseResponseDTO<bool> dt = new BaseResponseDTO<bool>();
+
+                if (string.IsNullOrEmpty(dto.Id))
+                {
+                    dt = await service.SaveReports(dto);
+                }
+                else
+                {
+                    dt = await service.UpdateReports(dto);
+                }
+                return Ok(dt);
+            }
+            catch (Exception)
+            {
+                return BadRequest();
+            }
+        }
+
+    }
+}
