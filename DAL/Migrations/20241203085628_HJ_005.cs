7e2d855a79e3a0b6aeff2c5ee1333d28b30e973a
hiraldevstree <127720048+hiraldevstree@users.noreply.github.com>
2024-12-03 14:36:26 +0530

    Zone Matrix changes

---
+++ DAL/Migrations/20241203085628_HJ_005.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class HJ_005 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "SYS_ZoneMatrix",
+                columns: table => new
+                {
+                    MID = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    IID = table.Column<long>(type: "bigint", nullable: false),
+                    GMID = table.Column<long>(type: "bigint", nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CreatedBy = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    UpdatedBy = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    UpdatedDate = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    DeleteStatus = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SYS_ZoneMatrix", x => x.MID);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SYS_ZoneMatrix");
+        }
+    }
+}
---
+++ DAL/Models/Administration/SYS_ZoneMatrix.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Models.Administration
+{
+    public class SYS_ZoneMatrix : BaseAuditModel, IAuditable
+    {
+        [Key]
+        public long MID { get; set; }
+        public long IID { get; set; } //Zone Id
+        public long GMID { get; set; }//MATRIX Id
+    }
+}
